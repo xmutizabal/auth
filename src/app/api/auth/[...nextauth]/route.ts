c0e118b59887ac1555bc7d7a9b7700b9ea7b0eb3
Cangu <xmutizabal@gmail.com>
2024-05-21 13:46:34 -0400

    Se termina signin con redirección. Además se muestran errores en formulario de registro.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
-
+import { connectDB } from "@/libs/mongodb";
+import User from "@/models/user";
+import bcrypt from 'bcryptjs'
 
 const handler = NextAuth({
     providers: [
@@ -11,11 +13,40 @@ const handler = NextAuth({
                 password: {label: "Password", type: "password", placeholder: "*********"}
             },
             async authorize(credentials, req) {
-                const user = {id: "1", fullname: "Xavi", email: "xavi@gmail.com"}
-                return user
-            },
+                await connectDB()
+                console.log(credentials)
+
+                const userFound = await User.findOne({email: credentials?.email}).select("+password")
+                if (!userFound) throw new Error("Invalid Credentials")
+
+                const passwordMatch = await bcrypt.compare(credentials!.password, userFound.password)
+                if (!passwordMatch) throw new Error("Invalid Credentials")
+                
+                console.log(userFound)
+                return userFound
+            }
         })
-    ]
+    ],
+    callbacks: {
+        jwt ({account, token, user, profile, session}) {
+            console.log({
+                account,
+                token,
+                user,
+                profile,
+                session
+            })
+            if (user) token.user = user
+            return token
+        },
+        session ({session,token}) {
+            session.user=token.user as any
+            return session
+        }
+    },
+    pages: {
+        signIn: "/login",
+    }
 })
 
 export { handler as GET, handler as POST }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,47 +1,57 @@
 "use client"
 
 import axios, {AxiosError} from "axios"
-import { FormEvent } from "react"
+import { FormEvent, useState } from "react"
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation"
 
 function RegisterPage() {
+  const [error, setError]= useState()
+  const router = useRouter()
 
-const handleSubmit = async (e: FormEvent<HTMLFormElement>)=>{
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
 
 
-  e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+
+    try {
+      const signUpResponse = await axios.post('/api/auth/signup', {
+        email: formData.get('email'),
+        password: formData.get('password'),
+        fullname: formData.get('fullname')
+        })
+      const res = await signIn ('credentials', {
+        email: signUpResponse.data.email,
+        password: formData.get("password"),
+        redirect: false,
+        })
+
+      if(res?.ok) return router.push("/dashboard")
+
+    } catch (error) { 
+        console.log(error)
+        if(error instanceof AxiosError){
+          setError(error.response?.data.message)
+        }
+    }
 
 
-  const formData = new FormData(e.currentTarget)
-
-  try {
-    const res = await axios.post('/api/auth/signup', {
-      email: formData.get('email'),
-      password: formData.get('password'),
-      fullname: formData.get('fullname')
-      })
-    console.log(res)
-  } catch (error) { 
-      console.log(error)
-      if(error instanceof AxiosError){
-        console.log(error.response?.data.message);
-      }
   }
+    return (
+      <div>
+        
+        <form onSubmit={handleSubmit}>
+          {error && <div className="bg-red-500 text-white p-2 mb-2"> {error} </div>}
+          <h1>Registro</h1>
+          <input type="text" placeholder="Nombre" name="fullname" className="bg-zinc-800 px-4 py-2 block mb-2"/>
+          <input type="text" placeholder="Email" name="email" className="bg-zinc-800 px-4 py-2 block mb-2" />
+          <input type="text" placeholder="*****" name="password" className="bg-zinc-800 px-4 py-2 block mb-2" />
+            <button className="bg-indigo-500 px-4 py-2 rounded-full">Registrarse</button>
+        </form>
 
-
-}
-  return (
-    <div>
-      
-      <form onSubmit={handleSubmit}>
-        <h1>Registro</h1>
-        <input type="text" placeholder="Nombre" name="fullname" className="bg-zinc-800 px-4 py-2 block mb-2"/>
-        <input type="text" placeholder="Email" name="email" className="bg-zinc-800 px-4 py-2 block mb-2" />
-        <input type="text" placeholder="*****" name="password" className="bg-zinc-800 px-4 py-2 block mb-2" />
-          <button className="bg-indigo-500 px-4 py-2 rounded-full">Registrarse</button>
-      </form>
-
-    </div>
-  )
-}
+      </div>
+    )
+  }
 
 export default RegisterPage
