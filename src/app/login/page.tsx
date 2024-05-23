e04faf20d397a7bf13b3030f6cbd7d46202fcc3c
Cangu <xmutizabal@gmail.com>
2024-05-23 19:47:45 -0400

    Login Page con formulario terminado

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,50 @@
+"use client"
+
+import { FormEvent, useState } from "react"
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation"
+
 function LoginPage() {
-  return (
-    <div>Login page</div>
-  )
-}
+  const [error, setError]= useState("")
+  const router = useRouter()
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+
+
+    const formData = new FormData(e.currentTarget)
+
+    try {
+
+      const res = await signIn ('credentials', {
+        email: formData.get('email'),
+        password: formData.get('password'),
+        redirect: false,
+        })
+
+      if(res?.error) return setError(res.error as string)
+
+      if(res?.ok) return router.push("/dashboard")
+
+    } catch (error) { 
+        console.log(error)
+    }
+
+
+  }
+    return (
+      <div>
+        
+        <form onSubmit={handleSubmit}>
+          {error && <div className="bg-red-500 text-white p-2 mb-2"> {error} </div>}
+          <h1>Login</h1>
+          <input type="text" placeholder="Email" name="email" className="bg-zinc-800 px-4 py-2 block mb-2" />
+          <input type="text" placeholder="*****" name="password" className="bg-zinc-800 px-4 py-2 block mb-2" />
+            <button className="bg-indigo-500 px-4 py-2 rounded-full">Entrar</button>
+        </form>
+
+      </div>
+    )
+  }
 
 export default LoginPage
